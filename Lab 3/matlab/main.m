162953eef21134cc4f3593386fc82e096a5dd12d
phielp <phielp@gmail.com>
2017-05-01 15:16:12 +0200

    initial Lab 3

---
+++ Lab 3/matlab/main.m
@@ -0,0 +1,40 @@
+%% Exercise 3
+% Philip Bouman (10668667) and Tomas Groot()
+
+function main()
+    close all
+    
+    image = rgb2gray(im2double(imread('shapes.png')));
+%     image = rgb2gray(im2double(imread('billboard.png')));
+%     image = rgb2gray(im2double(imread('box.png')));
+
+    %% Question 2
+    
+    % canny edge detector from lab 2
+	e1 = canny(image, 0.7);
+    
+    % built in canny edge detector
+	e2 = edge(image,'Canny');
+    
+    acc1 = hough(image, [-0.9,0.9],100, 100, 0);
+    acc2 = hough(image, [-0.9,0.9],100, 100, 1);
+    
+    figure;
+	subplot(2,2,1);
+	imshow(e1);
+    title('Canny edge detector from Lab 2');
+	subplot(2,2,2);
+	imshow(e2);
+    title('Built-in Canny edge detector');
+    subplot(2,2,3);
+    imshow(acc1, []);
+    title('Hough, using Canny Lab 2');
+    subplot(2,2,4);
+    imshow(acc2, []);
+    title('Hough, using built-in Canny');
+
+%% Question 3
+
+
+
+end
