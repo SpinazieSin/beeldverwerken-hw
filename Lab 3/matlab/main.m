c4fba8b43dfa7ea56cf9da0dc9e1aafbc20b176b
bakeraid <drone.grt@gmail.com>
2017-05-09 22:20:57 +0200

    finishing up 5

--- Lab 3/matlab/main.m
+++ Lab 3/matlab/main.m
@@ -1,9 +1,9 @@
 %% Exercise 3
-% Philip Bouman (10668667) and Tomas Groot()
+% Philip Bouman (10668667) and Thomas Groot(10658017)
 
 function main()
     close all
-    path = '../attachments/'
+    path = '../attachments/';
     image = rgb2gray(im2double(imread(strcat(path, 'shapes.png'))));
     %image = rgb2gray(im2double(imread(strcat(path, 'billboard.png'))));
     %image = rgb2gray(im2double(imread(strcat(path, 'box.png'))));
@@ -17,7 +17,7 @@ function main()
 	e2 = edge(image,'Canny');
     
     acc1 = hough(image, [-0.9,0.9],200, 200, 0);
-    acc2 = hough(image, [-0.9,0.9],100, 100, 1);
+    acc2 = hough(image, [-0.9,0.9],300, 300, 1);
 
     figure;
 	subplot(2,2,1);
@@ -37,27 +37,25 @@ function main()
 
     imshow(image);
     hold on
-    coords = houghlines(image, acc1, 0.4);
+    coords = houghlines(image, acc2, 0.4);
     hold off
     
-
     %% Question 5
 
-    line_list = zeros(size(coords,1), 3);
     % Restructure results of find
     [x,y] = find(e1);
     edge_points = [x,y];
     % Loop over end points
-    imshow(image);
     hold on
     for i = 1:length(coords)
+        % Get the cross product of the two end point vectors
         cross_vector = cross([coords(i,1),coords(i,2),1], [coords(i,3),coords(i,4),1]);
         norm_cross_prod = cross_vector/norm(cross_vector);
+        % Calculate the line points
         line_points = points_of_line(edge_points, norm_cross_prod, 1);
-        if size(line_points, 1) > 1
-            line = line_through_points(line_points);
-            plot(line)
-        end
+        % Least square fit of line points
+        line = line_through_points(line_points);
+        plot(line)
     end
     hold off
 end
