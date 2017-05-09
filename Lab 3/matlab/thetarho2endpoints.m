70eaddbd7b4ad867654188c250ab542549df0cf3
bakeraid <drone.grt@gmail.com>
2017-05-09 14:04:52 +0200

    fixed brackets

--- Lab 3/matlab/thetarho2endpoints.m
+++ Lab 3/matlab/thetarho2endpoints.m
@@ -3,15 +3,15 @@ function [x1, y1, x2, y2] = thetarho2endpoints(theta, rho, rows, cols)
     
     % If line is close to horizontal
     x1 = 0;
-    y1 = (x1 * sin(theta)-rho / cos(theta);
+    y1 = (x1 * sin(theta)-rho) / cos(theta);
     x2 = cols;
-    y2 = (x2 * sin(theta)-rho / cos(theta);
+    y2 = (x2 * sin(theta)-rho) / cos(theta);
     
     % If line is close to vertical
     if y2-y1 > rows
         y1 = 0;
-        x1 = (y1 * cos(theta)+rho / sin(theta);
+        x1 = (y1 * cos(theta)+rho) / sin(theta);
         y2 = rows;
-        x2 = (y2 * cos(theta)+rho / sin(theta);
+        x2 = (y2 * cos(theta)+rho) / sin(theta);
     end    
 end
