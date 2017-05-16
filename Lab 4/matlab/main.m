1bcf663ecc2f04803a702eb378aaebd2f5cae4b3
SpinazieSin <drone.grt@gmail.com>
2017-05-16 16:26:40 +0200

    SIFT matching points

--- Lab 4/matlab/main.m
+++ Lab 4/matlab/main.m
@@ -8,3 +8,24 @@ clear;
 %%
 demo_mosaic_alt(5);
 clear;
+
+%% Matching points with SIFT
+nacht1 = imread('nachtwacht1.jpg');
+nacht2 = imread('nachtwacht2.jpg');
+
+im1 = single(rgb2gray(nacht1));
+im2 = single(rgb2gray(nacht2));
+[F1, D1] = vl_sift(im1) ;
+[F2, D2] = vl_sift(im2) ;
+[matches, scores] = vl_ubcmatch(D1, D2);
+
+subplot(1,2,1);
+imshow(nacht1);
+hold on;
+plot(F1(1,matches(1,:)), F1(2, matches(1,:)), '*');
+subplot(1,2,2);
+imshow(nacht2);
+hold on
+plot(F2(1, matches(2,:)), F2(2, matches(2,:)), '*');
+hold off
+%%
