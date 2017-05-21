8a7e5ac2bf8a1df34181cd7bc263793e8c5ea671
SpinazieSin <drone.grt@gmail.com>
2017-05-21 14:13:08 +0200

    added data loading and reshaping functions

--- Lab 5/matlab/main.m
+++ Lab 5/matlab/main.m
@@ -4,4 +4,19 @@
 
 addpath('../attachments');
 
-%% 
+%% Load data and reshaping images
+data = load('omni.mat');
+
+% Create training set
+train_set = zeros(300, 16800);
+for i = 1:300
+    train_set(i, :) = reshape(data.images{1,i}.img, 1, 16800);
+end
+% Create testing set (test set index is index+training set size)
+test_set = zeros(250, 16800);
+for i = 1:250
+    train_set(i, :) = reshape(data.images{1,i+300}.img, 1, 16800);
+end
+
+%%
+
