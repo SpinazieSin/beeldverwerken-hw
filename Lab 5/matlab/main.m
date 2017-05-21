37357fa54cb38b1ba782c8f5a1d2b8d66f60876c
bakeraid <drone.grt@gmail.com>
2017-05-21 23:45:58 +0200

    lab 5

--- Lab 5/matlab/main.m
+++ Lab 5/matlab/main.m
@@ -21,13 +21,12 @@ for i = 1:250
     train_set(i, :) = reshape(data.images{1,i+300}.img, 1, 16800);
 end
 
-%% PCA
+%% Principal Component Analysis
 % NumComponents set the dimensionality of the coefficients
-[coeff, ~, eigenvalues] = pca(train_set, 'NumComponents', 300);
+[coeff, ~, eigenvalues] = pca(train_set, 'NumComponents', 50);
 for i = 1:size(coeff, 2)
     coeff(:, i) = coeff(:, i)/max(coeff(:, i));
 end
-%coeff = myPCA(train_set, 50);
 
 %% Plot first 5 PCA vectors as images
 
@@ -46,6 +45,52 @@ end
 
 plot(eigenvalues(1:50))
 
-%%
+%% Image similarity
 
+% This loop creates a matrix (image_index_list) of images that are most
+% similar to eachother.
+tic;
+image_index_list = zeros(2, (size(data.images, 2) - 250));
+for image = 1:(size(data.images, 2) - 250)
+    best_similarity = 0;
+    similar_image_index = 0;
+    for i = 1:size(coeff, 2)
+        similarity = dot(coeff(:, i), reshape(data.images{1,image}.img, 16800, 1));
+        if similarity < best_similarity && i ~= image
+            best_similarity = similarity;
+            similar_image_index = i;
+        end
+    end
+    % The image most similar to the image that is not itself is saved as i
+    image_index_list(:, image) = [image, similar_image_index];
+end
+toc
 
+%% Native image similarity
+
+% This loop creates a matrix (native_image_index_list) of images that are most
+% similar to eachother using the regular set of images instead of the PCA list.
+tic;
+native_image_index_list = zeros(2, (size(data.images, 2) - 250));
+for image = 1:(size(data.images, 2) - 250)
+    best_similarity = 0;
+    similar_image_index = 0;
+    for i = 1:(size(data.images, 2) - 250)
+        similarity = immse(reshape(data.images{1,i}.img, 1, 16800), reshape(data.images{1,image}.img, 1, 16800));
+        if similarity < best_similarity && i ~= image
+            best_similarity = similarity;
+            similar_image_index = i;
+        end
+    end
+    % The image most similar to the image that is not itself is saved as native_image_index_list
+    native_image_index_list(:, image) = [image, similar_image_index];
+end
+toc
+
+%% Positioning with Nearest Neighbour
+
+% Training and test set pca
+[train_set_coeff, score, eigenvalues] = pca([train_set; test_set], 'NumComponents', 50, 'Algorithm', 'svd');
+for i = 1:size(train_set_coeff, 2)
+    train_set_coeff(:, i) = train_set_coeff(:, i)/max(train_set_coeff(:, i));
+end
